38b05e91d18416780ed7d43423a426a8ed15cd77
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-06-21 23:34:04 +0100

    margin

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -23,11 +23,12 @@ const images = [
 const SliderWrapper = styled.div`
   position: relative;
   height: 100vh;
+  margin-bottom: 0 !important;
   overflow: hidden;
   box-shadow:
     rgba(133, 133, 192, 0.25) 0px 13px 27px -5px,
     rgba(159, 157, 157, 0.3) 0px 8px 16px -8px;
-  margin: auto;
+
   /* border-radius: 0 0 45px 45px; */
   @media screen and (max-width: 320px) {
     height: 120vh;
